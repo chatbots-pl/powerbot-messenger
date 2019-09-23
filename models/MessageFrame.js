cd1380405f75ad431ddd66c24106d4908f2d3bd4
Maksymilian Tomczyk <m.tomczyk.dev@gmail.com>
2019-09-23 13:12:21 +0200

    Issue #2 Fix

--- models/MessageFrame.js
+++ models/MessageFrame.js
@@ -2,7 +2,13 @@ class MessageFrame {
   constructor (message, recipient, options) {
     options = options || {}
     if (typeof (recipient) === 'object') options = recipient
-    this.messaging_type = options.messaging_type || 'RESPONSE'
+    if (typeof (options) === 'object') {
+      this.messaging_type = options.messaging_type || 'RESPONSE'
+      if (this.messaging_type === 'MESSAGE_TAG') this.tag = options.tag
+    } else if (typeof (options) === 'string') {
+      this.messaging_type = 'MESSAGE_TAG'
+      this.tag = options
+    }
     this.recipient = {}
     this.recipient.id = options.recipient_id || recipient
     this.message = message
