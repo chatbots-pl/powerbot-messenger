6fedc01bb143f8bbee614ea5fed01364c9586d71
Maksymilian Tomczyk <m.tomczyk.dev@gmail.com>
2019-12-18 02:24:07 +0100

    Account link and unlink buttons support

--- models/helpers/Button.js
+++ models/helpers/Button.js
@@ -2,12 +2,14 @@ const createError = require('../../modules/create_error')
 
 class Button {
   constructor (type, title, payload) {
-    let supported = ['web_url', 'postback', 'phone_number', 'element_share']
+    const supported = ['web_url', 'postback', 'phone_number', 'element_share', 'account_link', 'account_unlink']
+    const nonTitled = ['element_share', 'account_link', 'account_unlink']
     if (supported.indexOf(type) === -1) throw createError('This type of button is not currently supported by Powerbot Messenger. Please use raw send option instead.')
+    if (nonTitled.indexOf(type) === -1) this.title = title
     this.type = type
-    if (type !== 'element_share') this.title = title
     if (type === 'web_url') this.url = payload
     else if (type === 'postback' || type === 'phone_number') this.payload = payload
+    else if (type === 'account_link') this.url = title
   }
 }
 
